114f27a7d3af5fe32410400c1d66f4e3ac119585
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-03-23 00:06:18 -0700

    fixed a few minor errors

--- calculator.js
+++ calculator.js
@@ -1,98 +1,76 @@
 let total = 0;
-let auto = "0";
-let prevOp;
+let strbuffer = "0";
+let operator;
 const entry = document.querySelector(".result-screen");
 
-function click(value) {
-  if (isNaN(parseInt(value))) {
-    submit(value);
+function makesNumber(value) {
+    if (strbuffer === "0") {
+        strbuffer = value;
+    } else {
+        strbuffer += value;
+    }
+}
+
+function makesSymbol(symbol) {
+    if (symbol === "C") {
+        strbuffer = "0";
+        total = 0;
+    } else if (symbol === "=") {
+        if (operator === null) {
+            return;
+        }
+        mathOperations(parseInt(strbuffer));
+        operator = null;
+        strbuffer = +total;
+        total = 0;
+    } else if (symbol === "←") {
+        if (strbuffer.length === 1) {
+            strbuffer = "0";
+        } else {
+            strbuffer = strbuffer.substring(0, strbuffer.length - 1);
+        }
+    } else {
+        if (strbuffer === "0") {
+            return;
+          }
+        
+          const n = parseInt(strbuffer);
+          if (total === 0) {
+            total = n;
+          } else {
+            mathOperations(n);
+          }
+        
+          operator = symbol;
+          strbuffer = "0";
+    }
+}
+
+function mathOperations(n) {
+  if (operator === "+") {
+    total += n;
+  } else if (operator === "-") {
+    total -= n;
+  } else if (operator === "x") {
+    total *= n;
   } else {
-    addNum(value);
-  }
-  redisp();
-}
-
-function submit(value) {
-  switch (value) {
-    case "C":
-      auto = "0";
-      total = 0;
-      break;
-    case "=":
-      if (prevOp === null) {
-        return;
-      }
-      mathOp(parseInt(auto));
-      prevOp = null;
-      auto = +total;
-      total = 0;
-      break;
-    case "←":
-      if (auto.length === 1) {
-        auto = "0";
-      } else {
-        auto = auto.substring(0, auto.length - 1);
-      }
-      break;
-    case "+":
-    case "-":
-    case "×":
-    case "÷":
-      math(value);
-      break;
+    total /= n;
   }
 }
 
-
-function addNum(value) {
-  if (auto === "0") {
-    auto = value;
-  } else {
-    auto += value;
-  }
+function setListeners() {
+    document.querySelector(".calc-rows").addEventListener("click", function(event) {
+        buttonClicked(event.target.innerText);
+    });
 }
 
+setListeners();
 
-
-
-function redisp() {
-  entry.innerText = auto;
+function buttonClicked(value) {
+    if (isNaN(parseInt(value))) {
+        makesSymbol(value);
+    } else {
+        makesNumber(value);
+    }
+    entry.innerText = strbuffer;
 }
-
-function input() {
-  document.querySelector(".calc-rows").addEventListener("click", function(event) {
-    click(event.target.innerText);
-  });
-}
-
-
-function math(value) {
-  if (auto === "0") {
-    return;
-  }
-
-  const defNum = parseInt(auto);
-  if (total === 0) {
-    total = defNum;
-  } else {
-    mathOp(defNum);
-  }
-
-  prevOp = value;
-
-  auto = "0";
-}
-
-function mathOp(defNum) {
-  if (prevOp === "+") {
-    total += defNum;
-  } else if (prevOp === "-") {
-    total -= defNum;
-  } else if (prevOp === "×") {
-    total *= defNum;
-  } else {
-    total /= defNum;
-  }
-}
-
-input();
